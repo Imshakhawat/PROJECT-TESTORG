8e825335ca9a4b66c040d0784d0c84b72c97832d
mahinhossen <mahinhossen001@gmail.com>
2022-08-15 12:52:24 +0600

    deleted useless files from backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,21 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
 //controller functions
-const {signupUser, loginUser, verifyUser, resendMail} = require("../controllers/userController")
+const {
+  signupUser,
+  loginUser,
+  verifyUser,
+  resendMail,
+} = require("../controllers/userController");
 
 //signup
-router.post("/signup",signupUser)
+router.post("/signup", signupUser);
 //login
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 //mail confirmation
-router.get("/confirmation/:token",verifyUser);
-router.post("/confirmation/resend",resendMail);
+router.get("/confirmation/:token", verifyUser);
+router.post("/confirmation/resend", resendMail);
+//password recovery
 
 module.exports = router;
