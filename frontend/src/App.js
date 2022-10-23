6988168fb48770e2f6ef689a7d67571c09d21f05
mahinhossen <mahinhossen001@gmail.com>
2022-10-23 19:31:38 +0600

    changed login/signup in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,32 +11,56 @@ import Login from "./Pages/Login/Login";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [username, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const[usertype, setType] = useState("");
 
+//login
+/*     async function registerUser(e) {
+      e.preventDefault();
+      const response = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+    const data = await response.json();
+    console.log(data);
+  } */
+
+
+  //signup
   async function registerUser(e) {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        usertype,
+        username,
         email,
-        password,
+        password
       }),
     });
 
-    const data = await response.json();
-    console.log(data);
-  }
+  const data = await response.json();
+  console.log(data);
+}
 
   return (
-    <div className="App">
+    //login
+/*     <div className="App">
       <BrowserRouter>
         <div>
-          <h1>Register</h1>
+          <h1>Login</h1>
           <form onSubmit={registerUser}> 
             <input
               value={email}
@@ -52,7 +76,53 @@ function App() {
             />
             <input type="submit" value="Login" />
           </form>
+        </div> */
+
+
+
+
+
+
+        //signup
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <h1>SignUp</h1>
+          <form onSubmit={registerUser}> 
+            <input
+              value={username}
+              onChange={(e) => setName(e.target.value)}
+              type="username"
+              placeholder="username"
+            />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="email"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="password"
+            />
+            <input
+              value={usertype}
+              onChange={(e) => setType(e.target.value)}
+              type="usertype"
+              placeholder="usertype"
+            />
+            <input type="submit" value="SignUp" />
+          </form>
         </div>
+
+        
+
+
+
+
+
         {/* <Header></Header>
         <Switch>
           <Route exact path="/">
@@ -71,6 +141,10 @@ function App() {
         <Footer></Footer> */}
       </BrowserRouter>
     </div>
+
+
+
+
   );
 }
 
